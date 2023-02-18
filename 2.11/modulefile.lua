3b3191cd91f2d3da2f823b9a5aa2e83596e6ee4c
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2023-02-18 00:13:43 -0500

    Added version 2.11

---
+++ 2.11/modulefile.lua
@@ -0,0 +1,34 @@
+--
+-- visidata 2.11 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: VisiData is an interactive multitool for tabular data."
+
+whatis("Name: visidata")
+whatis("Version: 2.11")
+whatis("Category: Other")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: VisiData is an interactive multitool for tabular data.")
+
+help([[
+VisiData is an interactive multitool for tabular data.
+
+To load the module type
+
+> module load visidata/2.11
+
+To unload the module type
+
+> module unload visidata/2.11
+
+Tools included in this module are
+
+* vd
+]])
+
+local package = "visidata"
+local version = "2.11"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
+prepend_path("MANPATH", base)
